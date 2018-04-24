289f2c2440a845b445f47af15664256956c1778a
Sadra <ti9mail@gmail.com>
2018-04-24 17:20:45 +0200

    Add Spring structure

---
+++ accountancy/src/main/java/de/sadrab/accountancy/AccountancyApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.accountancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountancyApplication.class, args);
+	}
+}
