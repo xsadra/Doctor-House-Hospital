05bc339aa4da20eb99fa5a2cb2cc745ad0757a85
Sadra <ti9mail@gmail.com>
2018-04-24 14:04:32 +0200

    Add Spring Structure

---
+++ nursery/src/test/java/de/sadrab/nursery/NurseryApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sadrab.nursery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NurseryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
